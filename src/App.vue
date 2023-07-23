79c2e5c2d5c6ae3b54a67f2a7f3e3c33aceb6bd1
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-07-23 22:37:07 +0600

    carousel project full done

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,19 @@
 
     const images = reactive([
     {
+      thumb: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=150&q=60",
       full: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
     },
     {
+      thumb: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=150&q=60",
       full: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
     },
     {
+      thumb: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=150&q=60",
       full: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
     },
     {
+      thumb: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=150&q=40",
       full: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
     }
   ]);
@@ -38,11 +42,14 @@
 <template>
   
   <section class="container">
-    <h1 class="text-center text-2xl py-10">Image Carousel Project</h1>
+    <h1 class="text-center text-2xl py-10">Image Carousel Project</h1><br><br>
+    <div class="">
+        <img class="shadow-sm"  v-for="(image,index) in images" :src="image.thumb" :key="index" style="height:100px; margin:2px;" :style="index==imageIndex?'filter: blur(3px)':''">
+    </div><br>
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img :src="images[imageIndex].full" class="d-block w-100" alt="..." >
+        <img :src="images[imageIndex].full" class="d-block w-100" alt="..." style="height:500px;">
       </div>
     </div>
    <button class="carousel-control-prev" type="button" @click="imageSlideLeft()">
